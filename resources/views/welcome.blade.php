dd24982cbee36bd5cc8123a96e59181b466ed793
Andrei Lepekhov <lepekhov.andrei@gmail.com>
2021-11-19 15:42:16 +0200

    upt title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>2KINOFUN</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -33,7 +33,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    2Laravel2
+                    2KINOFUN
                 </div>
             </div>
         </div>
